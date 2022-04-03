9e687aae5145a803dc7a6105920386d4b2a5f97c
Alejandro Orbe <josealexorbech@hotmail.com>
2022-04-03 16:06:13 -0500

    a lot of changes

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,9 +3,22 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    # post views
+    # post views 
     path('', views.post_list, name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail,
          name='post_detail'),
 ]
+
+
+# from django.urls import include, re_path
+# from . import views
+
+# app_name = 'blog'
+# urlpatterns = [
+#     # post views
+#     re_path(r'', views.post_list, name='post_list'),
+#     re_path(r'<int:year>/<int:month>/<int:day>/<slug:post>/',
+#          views.post_detail,
+#          name='post_detail'),
+# ]
